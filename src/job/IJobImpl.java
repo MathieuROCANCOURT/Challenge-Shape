80a73e9f2037fe18922bfbaa3347afdbedd466f1
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-20 13:17:02 +0200

    chore: Add all methods from IJob interface in IJobImpl class.

--- src/job/IJobImpl.java
+++ src/job/IJobImpl.java
@@ -3,9 +3,47 @@
  */
 package job;
 
+import entities.Shape;
+
 /**
  * 
  */
-public class IJobImpl {
+public class IJobImpl implements IJob {
+
+	@Override
+	public void addShape(int id, Shape shape) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteShape(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Shape getShapeById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void moveShape(int id, int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawShape() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayAll() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
