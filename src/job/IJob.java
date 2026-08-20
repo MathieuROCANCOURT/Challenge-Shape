704369b4db8142fa9cd7ff07e3b483b7f88ee63e
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-20 13:07:28 +0200

    feat: Add methods in Ijob interface class.

--- src/job/IJob.java
+++ src/job/IJob.java
@@ -3,8 +3,21 @@
  */
 package job;
 
+import entities.Shape;
+
 /**
  * 
  */
 public interface IJob {
+	public void addShape(int id, Shape shape);
+
+	public void deleteShape(int id);
+
+	public Shape getShapeById(int id);
+
+	public void moveShape(int id, int x, int y);
+
+	public void drawShape();
+
+	public void displayAll();
 }
