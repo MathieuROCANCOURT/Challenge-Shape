1b269568326e9c07ae09cce4b5850592c7a4b8c4
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-20 11:35:29 +0200

    feat: Add Circle class.

---
+++ src/graphic/Circle.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package graphic;
+
+/**
+ * 
+ */
+import java.awt.Point;
+
+public class Circle extends Shape {
+	private double radius;
+
+	public Circle(double radius, int x, int y) {
+		super(x, y);
+		setRadius(radius);
+	}
+
+	public Circle(double radius, Point center) {
+		super(center);
+		setRadius(radius);
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		if (radius < 0)
+			this.radius = 13;
+		else
+			this.radius = radius;
+	}
+
+	@Override
+	public double area() {
+		return Math.PI * this.radius * this.radius;
+	}
+}
